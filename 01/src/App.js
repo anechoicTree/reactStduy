6e75305c6292b130f294b0b43a9a1c67b3d2978e
anechoicTree <anechoic77@gmail.com>
2023-08-25 11:20:55 +0900

    useState
    useState 사용법

--- 01/src/App.js
+++ 01/src/App.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React from 'react';
+import './styles.css';
 
-import "./styles.css";
 // don't change the Component name "App"
-function App() {
-  const printText = () => {
-    console.log('Stored!');
-  }
+// important: In this code editor, use React.useState() instead of just useState()
+export default function App(props) {
+  const prices = {
+    origin: 100,
+    disRate: 0.75
+  };
 
-  return <button onClick={printText}>Bookmark</button>;
+  const buttonTexts = {
+    origin: "할인 적용하기",
+    changed: "할인 적용 되돌리기"
+  };
+
+  const [price, setPrice] = React.useState(prices.origin);
+  const [buttonText, setButtonText] = React.useState(buttonTexts.origin);
+
+  const clickHandler = () => {
+    if (price === prices.origin) {
+      setPrice(prices.origin*prices.disRate);
+      setButtonText(buttonTexts.changed);
+    } else {
+      setPrice(prices.origin);
+      setButtonText(buttonTexts.origin);
+    }
+  };
+
+    return (
+        <div>
+            <p>${price}</p>
+            <button onClick={clickHandler}>{buttonText}</button>
+        </div>
+    );
 }
-
-export default App;
