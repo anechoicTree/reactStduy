efeb3954ab28f4898b877a09f40755d147d959bc
anechoicTree <anechoic77@gmail.com>
2023-08-28 11:38:40 +0900

    lifting state up
    자식에서 부모에게 전달하는~

--- 04/src/Components/NewExpenses/ExpenseForm copy.js
+++ 04/src/Components/NewExpenses/ExpenseForm copy.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './ExpenseForm.css';
-// import ExpenseDate from './../Expenses/ExpenseDate';
 
-const ExpenseForm = (props) => {
+const ExpenseForm = () => {
     // 별개로 관리
     const [enteredTitle, setEnteredTitle] = React.useState(''); 
     const [enteredAmount, setEnteredAmount] = React.useState('');
@@ -14,7 +13,6 @@ const ExpenseForm = (props) => {
     //     enteredAmount: '',
     //     selectedDate:''
     // });
-
 
     // document.getElementById('').addEventListener('click', (event) => {});
     const titleChangeHandler = (event) => {
@@ -45,47 +43,29 @@ const ExpenseForm = (props) => {
         // });
         // console.log(selectedDate);
     };
-
-    // const inputChangeHandler = (identifier, value) =>{
-    //     if (identifier ==='title'){
-    //       setEnterdTitle(value);
-    //     }else if(identifier === 'date'){
-    //       setEnterdDate(value);
-    //     }else{
-    //       setEnterdAmount(value);
-    //     };
-    //   };
-      
-      const submitHandler = (event) => {
-        event.preventDefault(); // 기본값에 동작안하게 해주는??
-        const expenseData = {
-          title: enteredTitle,
-          amount: enteredAmount,
-          date: new Date(selectedDate),
-        };
-
-        props.onSaveExpenseData(expenseData);
-
-        console.log(expenseData);
-        setEnteredTitle('');
-        setEnteredAmount('');
-        setSelectedDate('');
-      };
-
+    const inputChangeHandelr = (identifier, value) => {
+        if(identifier === 'title') {
+            setEnteredTitle(value);
+        } else if(identifier === 'date') {
+            setSelectedDate(value);
+        } else {
+            setEnteredAmount(value);
+        }
+    };
     return (
-    <form onSubmit={submitHandler}>
+    <form>
         <div className='new-expense__controls'>
             <div className='new-expense__control'>
                 <label>Title</label>
-                <input type='text' value={enteredTitle} onChange={titleChangeHandler}></input>
+                <input type='text' onChange={(event)=>{inputChangeHandelr('title',event.target.value)}}></input>
             </div>
             <div className='new-expense__control'>
                 <label>Amount</label>
-                <input type='number' min='0.01' step='0.01' value={enteredAmount} onChange={amountChangeHandler}></input>
+                <input type='number' min='0.01' step='0.01' onChange={(event)=>{inputChangeHandelr('amount',event.target.value)}}></input>
             </div>
             <div className='new-expense__control'>
                 <label>Date</label>
-                <input type='date' min='2019-01-01' max='2022-12-31' value={selectedDate} onChange={dateChangeHandler}></input>
+                <input type='date' min='2019-01-01' max='2022-12-31' onChange={(event)=>{inputChangeHandelr('date',event.target.value)}}></input>
             </div>
         </div>
         <div className='new-expense__actions'>
